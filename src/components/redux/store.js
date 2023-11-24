527de4002b7b0d905403541dcb60da9a89c8a949
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-11-24 18:58:46 +0100

    using combineReducers in store.js

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,14 +1,21 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import { contactsReducer } from './contactSlice.js';
 import { filterReducer } from './filterSlice';
 
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+const customizedMiddleware = (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  });
+
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
-  middleware: getDefaultMiddleware({
-    serializableCheck: { ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] },
-  }),
+  reducer: rootReducer,
+  middleware: customizedMiddleware,
 });
